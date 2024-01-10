b8c25d6e2795f730bd63862841daa177ad44eeab
khodary <ahmedmohamed2019.com@gmail.com>
2024-01-10 10:59:21 +0200

    show model bottom sheet

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -7,11 +8,18 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: const NotesViewBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
+        child: const Icon(Icons.add),
       ),
-      body: NotesViewBody(),
     );
   }
 }
+
