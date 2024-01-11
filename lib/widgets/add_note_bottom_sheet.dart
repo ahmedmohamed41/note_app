ccfb6879bcca51baf76e9b144c4c7f46174dde0b
khodary <ahmedmohamed2019.com@gmail.com>
2024-01-11 19:51:09 +0200

    add fileds to note bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_button.dart';
+import 'package:note_app/widgets/custom_text_filed.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -7,7 +8,30 @@ class AddNoteBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 85, 84, 84),
+        borderRadius: BorderRadius.circular(
+          20,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+        child: Column(
+          children: [
+            CustomTextField(
+              hintText: 'Title',
+              labelText: 'Title',
+            ),
+            CustomTextField(
+              hintText: 'content',
+              maxLines: 5,
+            ),
+            CustomButton(
+              text: 'add',
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/custom_text_filed.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomTextField extends StatelessWidget {
+  CustomTextField(
+      {super.key, this.labelText, this.hintText, this.onChang, this.inputType, this.maxLines=1});
+  String? labelText;
+  String? hintText;
+  int maxLines;
+  Function(String)? onChang;
+  TextInputType? inputType;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 5),
+      child: TextField(
+        onChanged: onChang,
+        keyboardType: inputType,
+        maxLines: maxLines,
+        style: const TextStyle(fontSize: 15.0),
+        decoration: InputDecoration(
+          labelText: labelText,
+          hintText: hintText,
+          // hintStyle: TextStyle(color: Colors.black),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(),
+          ),
+        ),
+      ),
+    );
+  }
+}
