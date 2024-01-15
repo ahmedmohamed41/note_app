b14f4de6bd230d3400a8ab11c60a0cd3bea06415
khodary <ahmedmohamed2019.com@gmail.com>
2024-01-15 19:45:47 +0200

    init hive and open note box

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 Color kPrimaryColor = const Color(0xFF5FEEE2);
 Color kBackGroundColor = const Color.fromARGB(255, 85, 84, 84);
+String kNotesBox = 'Notes box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/views/edit_note_view.dart';
 import 'package:note_app/views/notes_views.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
   runApp(const NoteApp());
 }
 
 class NoteApp extends StatelessWidget {
-  const NoteApp({super.key}); 
+  const NoteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
